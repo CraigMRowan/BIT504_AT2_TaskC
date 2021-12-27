6fce9f5c71d80a4f4ddbc7c0c86cc1318333c0e8
Craig Rowan <craigmrowan@gmail.com>
2021-12-27 16:11:39 +1300

    Added to Sprite class

--- Pong/src/Sprite.java
+++ Pong/src/Sprite.java
@@ -1,10 +1,12 @@
 import java.awt.Color;
+import java.awt.Rectangle;
 
 public class Sprite {
 	private int xPosition, yPosition;
 	private int xVelocity, yVelocity;
 	private int width, height;
 	private Color colour;
+	private int initialXPosition, initialYPosition;
 	
 	// Getter methods
 	public int getxPosition() {return xPosition;}
@@ -22,6 +24,22 @@ public class Sprite {
 	public void setyPosition(int yPosition) {
 		this.yPosition = yPosition;
 	}
+	public void setxPosition(int newX, int panelWidth) {
+		xPosition = newX;
+		if(xPosition < 0) {
+			xPosition = 0;
+		} else if(xPosition + width > panelWidth) {
+			xPosition = panelWidth - width;
+		}
+	}
+	public void setyPosition(int newY, int panelHeight) {
+		yPosition = newY;
+		if(yPosition < 0) {
+			yPosition = 0;
+		} else if(yPosition + height > panelHeight) {
+			yPosition = panelHeight - height;
+		}
+	}
 	public void setxVelocity(int xVelocity) {
 		this.xVelocity = xVelocity;
 	}
@@ -36,5 +54,19 @@ public class Sprite {
 	}
 	public void setColour(Color colour) {
         this.colour = colour;
+    }
+	
+	public void setInitialPosition(int initialX, int initialY) {
+		initialXPosition = initialX;
+		initialYPosition = initialY;
+	}
+	
+	public void resetToInitialPosition() {
+		setxPosition(initialXPosition);
+		setyPosition(initialYPosition);
+	}
+	
+	public Rectangle getRectangle() {
+        return new Rectangle(getxPosition(), getyPosition(), getWidth(), getHeight());
     }
 }
